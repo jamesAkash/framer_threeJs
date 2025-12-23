44eedffb6e9940abcc325c566a7dd720dcb183ce
jamesAkash <akash.james@outlook.com>
2025-12-23 21:56:40 +0530

    quick brush up framer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { Contact, Hero, Portfolio, Services } from "./components";
+import Test from "./components/Test";
 
 const App = () => {
   return (
-    <div className="container">
-      <section id="home">
-        <Hero />
-      </section>
-      <section id="services">
-        <Services />
-      </section>
-      <section id="portfolio">
-        <Portfolio />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-    </div>
+    // <div className="container">
+    //   <section id="home">
+    //     <Hero />
+    //   </section>
+    //   <section id="services">
+    //     <Services />
+    //   </section>
+    //   <section id="portfolio">
+    //     <Portfolio />
+    //   </section>
+    //   <section id="contact">
+    //     <Contact />
+    //   </section>
+    // </div>
+    <Test />
   );
 };
 
