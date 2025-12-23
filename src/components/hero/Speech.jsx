c9af30b120b5236916596b954e72e0997e3163b4
jamesAkash <akash.james@outlook.com>
2025-12-23 20:49:16 +0530

    adding react type animation

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,7 +1,26 @@
+import { TypeAnimation } from "react-type-animation";
+
 const Speech = () => {
   return (
     <div className="bubbleContainer">
-      <div className="bubble">Pokemon Introduction</div>
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            "We provide training for Picachus",
+            1000,
+            "We provide training for Bulbasaurs",
+            1000,
+            "We provide training for Charizards",
+            1000,
+            "And All Kind of Pokemons",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={60}
+          repeat={Infinity}
+        />
+      </div>
       <img src="/images/man.png" alt="man" />
     </div>
   );
