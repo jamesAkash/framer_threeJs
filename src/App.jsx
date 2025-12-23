47fe184c1e97d5d41751885f6fac354766502101
jamesAkash <akash.james@outlook.com>
2025-12-23 18:23:23 +0530

    folder structure for major component pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Contact, Hero, Portfolio, Services } from "./components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </div>
+  );
 };
 
 export default App;
